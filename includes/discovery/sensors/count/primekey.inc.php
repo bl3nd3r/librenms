44eb2ad956e9572b1027a55fead92a7ed6801dd1
Dan Kerse <daniel.kerse@gmail.com>
2022-02-20 11:46:13 +1300

    Add Copyright

--- includes/discovery/sensors/count/primekey.inc.php
+++ includes/discovery/sensors/count/primekey.inc.php
@@ -1,11 +1,22 @@
 <?php
-
 /*
+ * LibreNMS counter sensor for PrimeKey Hardware Appliances
  *
- * OIDs described here:
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ *
+ * The OIDs described here, for the EJBCA Appliance:
  * https://doc.primekey.com/ejbca-appliance/operations/webconf-configurator-of-hardware-appliance/monitoring
- *
-*/
+ * 
+ * @package    LibreNMS
+ * @subpackage webui
+ * @link       https://www.librenms.org
+ * @copyright  2022 LibreNMS
+ * @author     LibreNMS Contributors
+ */
 
 $oids = [
     0 => [
